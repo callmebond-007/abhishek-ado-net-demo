45267b6b94b13cce35d55f799db55a298e6b5e04
Abhishek singh <abhisheksinghme1@gmail.com>
2022-04-11 09:13:57 +0000

    Add project files.

---
+++ Lesson-1-ADO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Open a connection with the database.
+
+            string connectionString = @"data source=(localdb)\MSSQLLocalDB;database=StudentDB;integrated security=SSPI";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection is established!!!");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
